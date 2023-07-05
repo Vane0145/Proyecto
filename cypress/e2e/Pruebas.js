07157b7cbd083dbd0aa5f378ce154eae5de4b072
mi_vanessa <mi_canokarla034@gmail.com>
2023-07-05 09:53:35 -0600

    update

--- cypress/e2e/Pruebas.js
+++ cypress/e2e/Pruebas.js
@@ -2,4 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('template spec', () =>{
+    cy.visit('https://example.cypress.io')
+  })
 })
