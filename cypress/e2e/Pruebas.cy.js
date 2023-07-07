8e888949513eacc7bf4e593bf7257ec79d31cea4
mi_vanessa <mi_canokarla034@gmail.com>
2023-07-07 10:55:27 -0600

    update

---
+++ cypress/e2e/Pruebas.cy.js
@@ -0,0 +1,57 @@
+
+import homePage from "../pages/homePage"
+
+describe("Automation Proyect", () => {
+  beforeEach(() => {
+    cy.intercept('/service-worker.js', {
+      body: undefined
+    })
+    cy.clearAllSessionStorage({ log: true })
+    cy.visit('https://www.saucedemo.com')
+    cy.fixture("example").as("data")
+  
+  })
+
+  context("Login Section", () => {
+    it("Login incorrecto con Usuario o contraseña vacíos", () => {
+      cy.get("@data").then((data) => {
+        homePage.TypeEmailInput(data.emptyEmail)
+        cy.getByData("login-button").click()
+        homePage.TypePasswordInput(data.Password)
+        cy.getByData("error").should("exist")
+        console.log(data.Password)
+        console.log(data.emptyEmail)
+
+      })
+
+    })
+
+    it("Login incorrecto con password incorrecto", () => {
+      cy.get("@data").then((data) => {
+        homePage.TypeEmail(data.email)
+        homePage.TypePasswordInput(data.WrongPassword)
+        cy.getByData("login-button").click()
+        cy.getByData("error").should("exist")
+      })
+      
+    })
+
+    it("Login exitoso (standard_user)", () => {
+      cy.get("@data").then((data) => {
+        homePage.TypeEmail(data.email)
+        homePage.TypePasswordInput(data.Password)
+        cy.getByData("login-button").click()
+        cy.location("pathname").should("eq","/inventory.html")
+      })
+    })
+
+    
+  })
+
+
+})
+
+
+
+
+
---
+++ cypress/pages/homePage.js
@@ -0,0 +1,29 @@
+class homePage{
+
+    elements ={
+        EmailInput : () => cy.getByData("username"),
+        PasswordInput :() => cy.getByData("password"),
+        LoginBtn :() => cy.getByData("login-button"),
+        ErrorMessage :() => cy.getByData("error"),
+        
+         
+    }
+    TypeEmailInput(){
+        this.elements.EmailInput()
+    }
+    TypePasswordInput(password){
+        this.elements.PasswordInput().type(password)
+    }
+    clickLoginBtn(){
+        this.elements.LoginBtn().click()
+    }
+    AlertMessage(){
+        this.elements.AlertMessage()
+    }
+    TypeEmail(email){
+        this.elements.EmailInput(email).type(email)
+    }
+
+}
+
+module.exports = new homePage();
